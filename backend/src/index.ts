d62a476539ec0e57d10a636df89ce4bc3fab7a79
Evan-Coulter <evanrileycoulter@gmail.com>
2023-05-26 13:57:29 -0700

    add initial backend code

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, Express with TypeScript!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

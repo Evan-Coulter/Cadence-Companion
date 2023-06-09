03021dad29bab8b40636672d36468c0c8ffdac09
Evan Coulter <evanrileycoulter@gmail.com>
2023-06-08 19:52:03 -0700

    fix examples styling

--- frontend/src/Examples/index.tsx
+++ frontend/src/Examples/index.tsx
@@ -3,7 +3,7 @@ import Example from './Example'
 import './Examples.css'
 
 const Examples = () => {
-  const getSong = (artistName: string, songName: string) => {
+  const getSong = (songName: string, artistName: string) => {
     const song: CCSearchResponse = {
       songId: null,
       artistNames: [artistName],
@@ -13,21 +13,22 @@ const Examples = () => {
     }
     return song
   }
+  const examples: CCSearchResponse[] = [
+    getSong("“Die for you (Remix)”", "The Weeknd"),
+    getSong("“Lavender Haze”", "Taylor Swift"),
+    getSong("“Circles”", "Post Malone"),
+    getSong("“Family Ties”", "Kendrick Lamar"),
+    getSong("“Look at the Sky”", "Porter Robinson"),
+    getSong("“Nights Like This”", "Kehlani")
+  ]
 
   return (
   <section className='examples'>
     <div className='examples_inner'>
       <p className="examples_header">Examples</p>
-      <ul className='examples_inner_content'>
-        <Example song={getSong("Die for you (Remix)", "The Weeknd")}/>
-        <Example song={getSong("Lavender Haze", "Taylor Swift")}/>
-        <Example song={getSong("Circles", "Post Malone")}/>
-      </ul>
-      <ul className='examples_inner_content'>
-        <Example song={getSong("“Family Ties”", "Kendrick Lamar")}/>
-        <Example song={getSong("“Look at the Sky”", "Porter Robinson")}/>
-        <Example song={getSong("“Nights Like This”", "Kehlani")}/>
-      </ul>
+      <div className='examples_grid'>
+        {examples.map((example)=><Example song={example}/>)}
+      </div>
     </div>
   </section>)
 }
